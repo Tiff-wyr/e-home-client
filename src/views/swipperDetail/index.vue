9516c842a054ae69616bcc56a51ea3f28d7b4bd3
Tiff-wyr <18438610510@qq.com>
2018-10-19 09:46:08 +0800

    轮播图头部完成

--- src/views/swipperDetail/index.vue
+++ src/views/swipperDetail/index.vue
@@ -1,8 +1,19 @@
 <template>
+  <div>
+    <div class="message">
+      <div @click="$router.go(-1)">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-shangyiye"></use>
+        </svg>
+      </div>
+      <div class="title">{{biaoti}}</div>
+    </div>
     <div class="swipperDetail">
       <div class="title">{{formData.title}}</div>
       <div class="content" v-html="formData.content"></div>
     </div>
+  </div>
+
 </template>
 
 <script>
@@ -13,6 +24,7 @@
             formData:{
               title:'',
               content:'',
+              biaoti:'',
             }
           }
       },
@@ -23,18 +35,56 @@
                 this.formData=res.data
               }
             })
-          }
+          },
+          getTitle(id){
+            let title='';
+            switch (id){
+              case "2679" :
+                title = '随时随地学'
+                break
+              case "2656" :
+                title = '随时随地学'
+                break
+              case "2681" :
+                title = '信工新闻眼'
+                break
+              case "2592" :
+                title = '信工新闻眼'
+                break
+            }
+            this.biaoti=title
+        }
       },
       created(){
-          //router 有方法 push
-        //route 参数
           let id=this.$route.params.id
           this.getDetail(id)
+          this.getTitle(id)
       }
     }
 </script>
 
 <style scoped lang="scss">
+  .message{
+    width: 7.5rem;
+    background: #c50206;
+    .title{
+      text-align: center;
+      color: #fff;
+      height: 0.86rem;
+      line-height: 0.86rem;
+      width: 7.1rem;
+      margin: 0 auto;
+      font-size: 0.34rem;
+    }
+    .icon{
+      width: 0.9rem;
+      height: 0.9rem;
+      color: #fff;
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+  }
 .title{
    font-size: 0.5rem;
   font-weight: bold;
